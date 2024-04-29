78d272f46ebd667f4d1fac50f2744e9ba34f50a4
Thomas Dutouquet <140429626+tdutouquet@users.noreply.github.com>
2024-04-29 14:54:57 +0200

    Admin fixture

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher,
+    ) {}
+    
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setEmail('admin@localhost.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword(
+            $this->passwordHasher->hashPassword($admin,'admin')
+        );
+
+        $manager->persist($admin);
+        $manager->flush();
+    }
+}
